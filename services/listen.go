062f41c2efb98819b2f3e70ff9f574e780da19f1
hteen <hteen@qq.com>
2021-10-12 10:34:16 +0800

    增加请求头和日本地区

--- services/listen.go
+++ services/listen.go
@@ -201,8 +201,12 @@ func  (s *listenService) groupByStore() map[string]bool {
 func (s *listenService) getSkuByLink(ch chan map[string]bool, skUrl string) {
     skus := map[string]bool{}
     
-    resp, body, errs := gorequest.New().Timeout(time.Second*3).Get(skUrl).End()
+    resp, body, errs := gorequest.New().
+        Set("referer", "https://www.apple.com/shop/buy-iphone").
+        Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36").
+        Timeout(time.Second*3).Get(skUrl).End()
     if len(errs) > 0 {
+        log.Println(errs)
         ch <- skus
         return
     }
